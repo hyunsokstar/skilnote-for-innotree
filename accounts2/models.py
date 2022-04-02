eb8d2a7ddb2876217973552aba80ee4c49b41fa9
hyun <hyunsokstar@gmail.com>
2022-04-02 17:52:53 +0900

    update for login page

--- accounts2/models.py
+++ accounts2/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(    User, on_delete=models.CASCADE, related_name='profile')
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
     completecount = models.IntegerField(default=0)
@@ -13,7 +13,6 @@ class Profile(models.Model):
     shortcut_user_id = models.CharField(default="me", max_length=40)
     selected_category_id = models.IntegerField(default=1, blank=True)
     position = models.CharField(max_length=50,default="member")
-    subject_of_memo = models.CharField(max_length=60) # 스킬 노트의 주제
     skill_note_reputation = models.IntegerField(default=0) # skill note 유저 리스트 점수 추가할때 계산됨
     email = models.CharField(max_length=20 , blank=True)
     public = models.CharField(max_length=5 , default="yes")
@@ -22,3 +21,5 @@ class Profile(models.Model):
     site1 = models.CharField(max_length=20 , blank=True)
     site3 = models.CharField(max_length=20 , blank=True)
     site4 = models.CharField(max_length=20 , blank=True)
+    # subject_of_memo = models.CharField(max_length=60) # 스킬 노트의 주제
+    subject = models.CharField(max_length=60, blank=True, null=True, default="back")
